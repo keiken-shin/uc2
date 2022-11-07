42a41409d87ad8e319c173a7607586dcb508f6a4
keiken <anubhav.kumar27@gmail.com>
2022-11-07 19:51:54 +0530

    Intialized React project with lint & tailwind

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import logo from './assets/logo.png';
+
+function App() {
+  return (
+    <div className="bg-red-100">
+      <img src={logo} alt="logo" />
+      <h1>Legal AI</h1>
+    </div>
+  );
+}
+
+export default App;
